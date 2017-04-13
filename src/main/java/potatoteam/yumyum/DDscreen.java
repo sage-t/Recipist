94a0f3c51963e9dc73c2cf1a9d2c7959495a9923
qidu0919 <qzd919@gmail.com>
2017-04-13 15:09:37 -0600

    Update DDscreen.java

--- src/main/java/potatoteam/yumyum/DDscreen.java
+++ src/main/java/potatoteam/yumyum/DDscreen.java
@@ -1,13 +1,28 @@
-package potatoteam.yumyum;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class DDscreen extends AppCompatActivity {
     public Button but2;
+    EditText editText = (EditText) findViewById(R.id.search);
+    editText.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent){
+            boolean handled = false;
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                sendMessage();
+                handled = true;
+            }
+            return handled;
+        }
+        });
+
     public void init (){
         but2 = (Button)findViewById(R.id.but2);
         but2.setOnClickListener(new View.OnClickListener() {
@@ -24,4 +39,6 @@ public class DDscreen extends AppCompatActivity {
         setContentView(R.layout.activity_ddscreen);
         init();
     }
+
+
 }
