a29be1bc4ba2b6229e9cc359e07a9e8f1c91d8a8
qidu0919 <qzd919@gmail.com>
2017-04-13 15:56:28 -0600

    Update DDscreen.java

--- src/main/java/potatoteam/yumyum/DDscreen.java
+++ src/main/java/potatoteam/yumyum/DDscreen.java
@@ -1,27 +1,28 @@
+package potatoteam.yumyum;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
 
-public class DDscreen extends AppCompatActivity {
+public class DDscreen extends AppCompatActivity implements OnEditorActionListener {
     public Button but2;
     EditText editText = (EditText) findViewById(R.id.search);
-    editText.setOnEditorActionListener(new TextView.OnEditorActionListener(){
-        @Override
-        public boolean onEditorAction(TextView v, int actionId, KeyEvent){
-            boolean handled = false;
-            if (actionId == EditorInfo.IME_ACTION_SEND) {
-                sendMessage();
-                handled = true;
-            }
-            return handled;
-        }
-        });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        editText.setOnEditorActionListener(this);
+        setContentView(R.layout.activity_ddscreen);
+        init();
+    }
 
     public void init (){
         but2 = (Button)findViewById(R.id.but2);
@@ -33,11 +34,15 @@ public class DDscreen extends AppCompatActivity {
             }
         });
     }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ddscreen);
-        init();
+
+
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event){
+        boolean handled = false;
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            Toast.makeText(this,"HOFEFDJBKFDSSBKJFDS", Toast.LENGTH_LONG);
+            handled = true;
+        }
+        return handled;
     }
 
 
