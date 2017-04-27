fd20e0111bfe23de58ff08d868aaf282f6aa145d
viso4661 <viso4661@colorado.edu>
2017-04-27 14:40:27 -0600

    Update Main.java
    working api calls prints to screen, next step is to make it look pretty!

--- src/main/java/potatoteam/yumyum/Main.java
+++ src/main/java/potatoteam/yumyum/Main.java
@@ -1,39 +1,69 @@
-package potatoteam.yumyum;
+package qzd919.recipist;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Main extends AppCompatActivity {
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//import org.json.JSONTokener;
 
-    public Button but1;
-    public void init (){
-        but1 = (Button)findViewById(R.id.but1);
-        but1.setOnClickListener(new View.OnClickListener() {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+//code structure from http://www.androidauthority.com/use-remote-web-api-within-android-app-617869/
+public class MainActivity extends AppCompatActivity { //search for ingredients for now.
+
+    EditText ingredientText;
+    TextView responseView;
+
+    String ingredient;
+    //ProgressBar progressBar;
+    //static final String API_KEY = "USE_YOUR_OWN_API_KEY";
+    static final String API_URL = "http://recipist-csci3308.herokuapp.com/search?ingrds=";
+    // noticed the api only takes in lower-case words
+    //multi-search only matches 1 ingredient
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        responseView = (TextView) findViewById(R.id.responseView);
+        ingredientText = (EditText) findViewById(R.id.ingredientText);
+
+    // got it from: http://stackoverflow.com/questions/32568340/android-studio-error-method-gettext-must-be-called-from-the-ui-thread-curre
+        Button queryButton = (Button) findViewById(R.id.queryButton);
+        queryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent toy = new Intent(Main.this, DDscreen.class);
-                startActivity(toy);
+                ingredient = ingredientText.getText().toString();
+
+                new RetrieveFeedTask().execute();
             }
         });
     }
-class RetrieveFeedTask extends AsyncTask<Void, Void, String> { // maybe a way to retrive the api? http://www.androidauthority.com/use-remote-web-api-within-android-app-617869/
 
-        private Exception exception;
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+       // private Exception exception;
 
         protected void onPreExecute() {
-            progressBar.setVisibility(View.VISIBLE);
+
             responseView.setText("");
         }
 
         protected String doInBackground(Void... urls) {
-            String email = emailText.getText().toString();
-            // Do some validation here
-
             try {
-                URL url = new URL(API_URL + "i forgot" + email + "what the api was=" + API_KEY);
+                URL url = new URL(API_URL + ingredient);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -50,25 +80,18 @@ class RetrieveFeedTask extends AsyncTask<Void, Void, String> { // maybe a way to
                 }
             }
             catch(Exception e) {
-                Log.e("error", e.getMessage(), e);
+                Log.e("ERROR", e.getMessage(), e);
                 return null;
             }
         }
-
+        // some error handling
         protected void onPostExecute(String response) {
             if(response == null) {
-                response = "error fetching";
+                response = "THERE WAS AN ERROR";
             }
-            progressBar.setVisibility(View.GONE);
-            Log.i("info", response);
+            Log.i("INFO", response);
             responseView.setText(response);
-        }
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        init();
 
+        }
     }
 }
